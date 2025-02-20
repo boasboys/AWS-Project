23eae67fb5eaae1bc866a3ffc894e881ab64c861
Big Hunter <164090451+boasboys@users.noreply.github.com>
2025-02-20 14:35:44 +0200

    Delete frontend directory

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import WafTreeDagreDetailed from "./components/WafTreeDagreDetailed";
-import WafTreeIfElseDetailed from "./components/WafTreeIfElseDetailed";
-import WafTree from "./components/WafTree";
-
-
-export default function App() {
-  return <WafTreeDagreDetailed />;
-}
